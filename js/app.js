7b2c603004bccab111a4a7d6f8b606e8d7b50322
jbenjaminy <young.benjamin@gmail.com>
2016-06-15 17:41:28 -0500

    Adds bug fix

--- js/app.js
+++ js/app.js
@@ -1,22 +1,24 @@
-'use strict';
     $(document).ready(function() {
         console.log(fizzBuzz());
-        event.preventDefault();
     });
 
     var fizzBuzz = function() {
-        event.preventDefault();
 
         var userInput = parseInt(prompt("Enter a number"));
+        var output = "";
         for (var i = 1; i <= userInput; i++ ) {
             if((i % 3 === 0) && (i % 5 === 0)) {
-                document.write("fizzbuzz <br>");}
-            else if (i % 3 === 0) {
-                document.write("fizz <br>");}
-            else if (i % 5 === 0) {
-                document.write("buzz <br>");}
-            else {
-                document.write(i + "<br>");
+                output = "fizzbuzz <br>";
             }
+            else if (i % 3 === 0) {
+                output = "fizz <br>";
+            }
+            else if (i % 5 === 0) {
+                output = "buzz <br>";
+            }
+            else {
+                output = i + "<br>";
+            }
+            $('#results').append(output);
         }
     }
